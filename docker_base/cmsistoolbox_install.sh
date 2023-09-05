4abc91abf2d1bbd856e059c378a09ea040e4944e
Matthias Hertel <matthias.hertel@arm.com>
2023-09-05 10:24:52 +0200

    Add all tools

---
+++ docker_base/cmsistoolbox_install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Open-CMSIS-Pack/cmsis-toolbox/releases/download/2.1.0/cmsis-toolbox-linux-amd64.tar.gz -nv
+tar -xf cmsis-toolbox-linux-amd64.tar.gz -C $HOME
+export PATH=$HOME/cmsis-toolbox/bin:$PATH

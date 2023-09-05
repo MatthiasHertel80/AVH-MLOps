f17f348181fc2b07362e4160f2ebfdb8b5cc342f
Matthias Hertel <matthias.hertel@arm.com>
2023-09-05 14:40:53 +0200

    Update cmsistoolbox_install.sh

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -2,9 +2,9 @@ wget https://github.com/Open-CMSIS-Pack/cmsis-toolbox/releases/download/2.1.0/cm
 apt remove -y cmake
 snap install cmake
 snap install ninja-build
-tar -xf cmsis-toolbox-linux-amd64.tar.gz -C $HOME
-export AC6_TOOLCHAIN_6_20_1=$HOME/ArmCompilerforEmbedded6.20.1/bin
-export CMSIS_PACK_ROOT=$HOME/packs
-export CMSIS_COMPILER_ROOT=$HOME/cmsis-toolbox-linux-amd64/etc
-export PATH=$HOME/cmsis-toolbox-linux-arm64/bin:$PATH
+tar -xf cmsis-toolbox-linux-amd64.tar.gz -C /home/arm_mlops_docker
+export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin
+export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs
+export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc
+export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-arm64/bin:$PATH
 cpackget init https://www.keil.com/pack/index.pidx

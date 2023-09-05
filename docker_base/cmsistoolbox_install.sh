eba172f33655b9a7441693bcee752a34b39039f6
Matthias Hertel <matthias.hertel@arm.com>
2023-09-05 16:07:29 +0200

    Update cmsistoolbox_install.sh

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -4,8 +4,9 @@ tar -xf cmsis-toolbox-linux-amd64.tar.gz -C /home/arm_mlops_docker
 chmod +x cmake-3.27.4-linux-x86_64.sh
 mkdir /home/arm_mlops_docker/cmake
 ./cmake-3.27.4-linux-x86_64.sh --skip-license --prefix=/home/arm_mlops_docker/cmake
-su -l arm_mlops_docker -c "export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin"
-su -l arm_mlops_docker -c "export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs"
-su -l arm_mlops_docker -c "export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc"
-su -l arm_mlops_docker -c "export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-arm64/bin:/home/arm_mlops_docker/cmake/bin:$PATH"
-su -l arm_mlops_docker -c "cpackget init https://www.keil.com/pack/index.pidx"
+su -l arm_mlops_docker  
+export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin
+export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs
+export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc
+export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/bin:/home/arm_mlops_docker/cmake/bin:$PATH
+cpackget init https://www.keil.com/pack/index.pidx

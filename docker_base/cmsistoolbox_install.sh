57a9504092092a828925e6702730c2ffb1444169
Matthias Hertel <matthias.hertel@arm.com>
2023-09-05 16:55:15 +0200

    Update cmsistoolbox_install.sh

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -4,6 +4,7 @@ tar -xf cmsis-toolbox-linux-amd64.tar.gz -C /home/arm_mlops_docker
 chmod +x cmake-3.27.4-linux-x86_64.sh
 mkdir /home/arm_mlops_docker/cmake
 ./cmake-3.27.4-linux-x86_64.sh --skip-license --prefix=/home/arm_mlops_docker/cmake
+apt install ninja-build
 su -l arm_mlops_docker  
 export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin
 export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs

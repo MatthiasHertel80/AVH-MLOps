b14534961b06c9cba078f9beaa4444826f4baa20
Matthias Hertel <matthias.hertel@arm.com>
2023-09-05 11:05:41 +0200

    New image test

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -1,3 +1,10 @@
 wget https://github.com/Open-CMSIS-Pack/cmsis-toolbox/releases/download/2.1.0/cmsis-toolbox-linux-amd64.tar.gz -nv
+apt remove -y cmake
+snap install cmake
+snap install ninja-build
 tar -xf cmsis-toolbox-linux-amd64.tar.gz -C $HOME
-export PATH=$HOME/cmsis-toolbox/bin:$PATH
+export AC6_TOOLCHAIN_6_20_1=$HOME/ArmCompilerforEmbedded6.20.1/bin
+export CMSIS_PACK_ROOT=$HOME/packs
+export CMSIS_COMPILER_ROOT=$HOME/cmsis-toolbox-linux-amd64/etc
+export PATH=$HOME/cmsis-toolbox-linux-arm64/bin:$PATH
+cpackget init https://www.keil.com/pack/index.pidx

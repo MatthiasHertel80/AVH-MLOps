0695b3ef2d746233d89c9f9398ec6d6653cdf77b
Matthias Hertel <matthias.hertel@arm.com>
2023-09-06 09:23:16 +0200

    Update cmsistoolbox_install.sh

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -10,4 +10,4 @@ echo 'export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs' >> /home/arm_mlops_do
 echo 'export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc' >> /home/arm_mlops_docker/.bashrc
 echo 'export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/bin:$PATH' >> /home/arm_mlops_docker/.bashrc
 echo 'export PATH=/home/arm_mlops_docker/cmake/bin/:$PATH' >> /home/arm_mlops_docker/.bashrc
-su -l arm_mlops_docker -c "cpackget init https://www.keil.com/pack/index.pidx"
+su -l arm_mlops_docker -c "source /home/arm_mlops_docker/.bashrc && cpackget init https://www.keil.com/pack/index.pidx"

a3ff3c6c263a7868f2074e3c7d3562cd5c652c23
Matthias Hertel <matthias.hertel@arm.com>
2023-09-06 09:00:28 +0200

    Update cmsistoolbox_install.sh

--- docker_base/cmsistoolbox_install.sh
+++ docker_base/cmsistoolbox_install.sh
@@ -4,10 +4,10 @@ tar -xf cmsis-toolbox-linux-amd64.tar.gz -C /home/arm_mlops_docker
 chmod +x cmake-3.27.4-linux-x86_64.sh
 mkdir /home/arm_mlops_docker/cmake
 ./cmake-3.27.4-linux-x86_64.sh --skip-license --prefix=/home/arm_mlops_docker/cmake
-apt install ninja-build
-su -l arm_mlops_docker  
-export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin
-export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs
-export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc
-export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/bin:/home/arm_mlops_docker/cmake/bin:$PATH
+apt install ninja-build -y
+echo 'export AC6_TOOLCHAIN_6_20_1=/home/arm_mlops_docker/ArmCompilerforEmbedded6.20.1/bin' >> /home/arm_mlops_docker/.bashrc
+echo 'export CMSIS_PACK_ROOT=/home/arm_mlops_docker/packs' >> /home/arm_mlops_docker/.bashrc
+echo 'export CMSIS_COMPILER_ROOT=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/etc' >> /home/arm_mlops_docker/.bashrc
+echo 'export PATH=/home/arm_mlops_docker/cmsis-toolbox-linux-amd64/bin:$PATH' >> /home/arm_mlops_docker/.bashrc
+echo 'export PATH=/home/arm_mlops_docker/cmake/bin:$PATH' >> /home/arm_mlops_docker/.bashrc
 cpackget init https://www.keil.com/pack/index.pidx
